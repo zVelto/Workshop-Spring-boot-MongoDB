61c8226c1976e84fac6942392f0b0724d15e2bbd
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-25 20:04:46 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/zVelto/workshopmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.zVelto.workshopmongo.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.zVelto.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(
+				System.currentTimeMillis(), 
+				status.value(), 
+				"Não encontrado", 
+				e.getMessage(), 
+				request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/com/zVelto/workshopmongo/services/UserService.java
+++ src/main/java/com/zVelto/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.zVelto.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.zVelto.workshopmongo.domain.User;
 import com.zVelto.workshopmongo.repositories.UserRepository;
+import com.zVelto.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
