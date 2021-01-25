8941d8c7bfd7856eda720ac43ba6bfc05baa9868
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-25 20:32:49 -0300

    Inserção de usuário com POST

--- src/main/java/com/zVelto/workshopmongo/services/UserService.java
+++ src/main/java/com/zVelto/workshopmongo/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.zVelto.workshopmongo.domain.User;
 import com.zVelto.workshopmongo.repositories.UserRepository;
 import com.zVelto.workshopmongo.services.exceptions.ObjectNotFoundException;
+import com.zVelto.workshopmongodto.UserDTO;
 
 @Service
 public class UserService {
@@ -24,5 +25,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
