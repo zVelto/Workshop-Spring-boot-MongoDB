d92706ff9c414a2256c589fa37eb5bb80d20b20c
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-27 14:02:27 -0300

    Obtendo um post por id

---
+++ src/main/java/com/zVelto/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.zVelto.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zVelto.workshopmongo.domain.Post;
+import com.zVelto.workshopmongo.repositories.PostRepository;
+import com.zVelto.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
